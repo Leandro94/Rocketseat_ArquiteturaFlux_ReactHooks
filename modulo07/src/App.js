b7ec6cbc2f8721d6968f444f19ed0bcae187832e
LeandroR94 <reisleandro868@gmail.com>
2019-10-23 16:59:11 -0300

    Estilos globais

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-    
-    <Routes/>
+      <Routes />
+      <GlobalStyle />
     </BrowserRouter>
-  )
+  );
 }
 export default App;
