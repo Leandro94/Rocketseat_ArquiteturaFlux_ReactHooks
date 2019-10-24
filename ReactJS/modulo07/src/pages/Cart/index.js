c827fe051370e8c3459d706a35f38f5d44527d01
LeandroR94 <reisleandro868@gmail.com>
2019-10-24 12:01:01 -0300

    Estilização do Carrinho

--- ReactJS/modulo07/src/pages/Cart/index.js
+++ ReactJS/modulo07/src/pages/Cart/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-feminino/26/COL-4398-026/COL-4398-026_detalhe2.jpg?ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tènis Adidas Falcon</strong>
+              <span>R$155,99</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$320,00</strong>
+            </td>
+            <t>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </t>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
