199f56b64cb744eeb9528c47550b9e029be47faa
LeandroR94 <reisleandro868@gmail.com>
2019-11-01 09:58:50 -0300

    Hook useMemo

--- ReactJS/modulo08/src/App.js
+++ ReactJS/modulo08/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [tech, setTech] = useState(['ReactJS', 'React Native']);
@@ -16,6 +16,7 @@ function App() {
   useEffect(() => {
     localStorage.setItem('tech', JSON.stringify(tech));
   }, [tech]);
+  const techSize = useMemo(() => tech.length, [tech]);
   return (
     <>
       <ul>
@@ -23,6 +24,8 @@ function App() {
           <li>{t}</li>
         ))}
       </ul>
+      <strong>Voce tem {techSize} tecnologias</strong>
+      <br />
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
